641ff01ae0011bf38d0ecf2ccf446065002820d2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-02-04 12:06:07 +0100

    genrate docs from roxygen2

--- R/clere-package.R
+++ R/clere-package.R
@@ -49,6 +49,16 @@
 #' 
 NULL
 
+## usethis namespace: start
+#' @useDynLib clere, .registration = TRUE
+## usethis namespace: end
+NULL
 
-
-
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+## usethis namespace: start
+#' @import RcppEigen
+## usethis namespace: end
+NULL
